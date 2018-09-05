ea67b164a93e9f0313e7836ba31e2f4823d8fbe7
Susie <susie.kim@hotmail.com>
2018-09-05 16:12:56 -0400

    added inital components

---
+++ src/Components/Landing.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import SearchForm from './SearchForm';
+
+class Landing extends Component {
+    render() {
+        return (
+            <div className="landing-page">
+                <h1>CRITTER</h1>
+                <h2>Find your creepy companion</h2>
+                <SearchForm />
+            </div>
+        )
+    }
+}
+
+export default Landing;
