e8d6f1eaa0c75cd0a95873189995dc67b5afc983
Susie <susie.kim@hotmail.com>
2018-09-05 16:51:47 -0400

    got axios working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
 import Landing from './Components/Landing';
+import axios from 'axios';
+import Qs from 'qs';
 
 class App extends Component {
+    getPets = (location, type, age) => {
+        // return (
+            console.log(location, type, age);
+            axios({
+                url: 'https://proxy.hackeryou.com',
+                method: 'GET',
+                dataResponse: 'json',
+                paramsSerializer: function (params) {
+                  return Qs.stringify(params, {
+                    arrayFormat: 'brackets'
+                  })
+                },
+                params: {
+                  reqUrl: 'https://api.petfinder.com/pet.find',
+                  params: {
+                    key: '729776b0ff12f97426ef03d015026841',
+                    format: 'json',
+                    output: 'full',
+                    location: location,
+                    animal: type,
+                    age: age,
+                    count: 100,
+                  },
+                  proxyHeaders: {
+                    'header_params': 'value'
+                  },
+                  xmlToJSON: false
+                }
+            }).then((res) => {
+                console.log(res.data.petfinder.pets);
+            })
+        // )
+    }
+    
     render() {
         return (
             <div className="App">
-                <Landing />
+                <Landing getPets={this.getPets}/>
             </div>
         );
     }
--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
 
-class Landing extends Component {
+class SearchForm extends Component {
+    constructor() {
+        super();
+        this.state = {
+            location: '',
+            type: '',
+            age: ''
+
+        };
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        console.log('submitted');
+        e.preventDefault();
+        let location = this.state.location;
+        let type = this.state.type;
+        let age = this.state.age;
+        if (location.length > 0 && type.length > 0 && age.length > 0) {
+            this.props.getPets(location, type, age);
+        } else {
+            alert('FILL OUT THE FORM!!');
+        }
+    }
+
     render() {
         return (
-            <form action="">
-                <input type="text" placeholder="enter location"/>
-                <select name="" id="">
-                    <option value="">Reptiles</option>
-                    <option value="">Small furries</option>
-                    <option value="">Fish</option>
-                    <option value="">Birds</option>
-                    <option value="">Other</option>
+            <form action="" onSubmit={this.handleSubmit}>
+                <input type="text" id="location" placeholder="Enter City and Province" onChange={this.handleChange}/>
+                <select name="types" id="type" onChange={this.handleChange}>
+                    <option value="" selected disabled hidden>Type</option>
+                    <option value="reptile">Reptile</option>
+                    <option value="smallfurry">Small and Furry</option>
+                    <option value="bird">Flying Friends</option>
                 </select>
-                <select name="" id="">
-                    <option value="">Baby</option>
-                    <option value="">Juvenile</option>
-                    <option value="">Senior</option>
+                <select name="" id="age" onChange={this.handleChange}>
+                    <option value="" selected disabled hidden>Age</option>
+                    <option value="Baby">Lil Bebes</option>
+                    <option value="Young">Youngins</option>
+                    <option value="Adult">Grown ups</option>
+                    <option value="Senior">Wrinkly boys</option>
                 </select>
                 <input type="submit" value="FIND YO PET"/>
             </form>
