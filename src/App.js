51433c9b0ff8859ebafab207e2e45596b537dac6
kandacewithak <kandacehobrien@gmail.com>
2018-09-06 14:31:33 -0400

    thursday changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Landing from './Components/Landing';
+import axios from 'axios';
+import Qs from 'qs';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    getPets = (location, type, age) => {
+        // return (
+            console.log(location, type, age);
+            axios({
+                url: 'https://proxy.hackeryou.com',
+                method: 'GET',
+                dataResponse: 'json',
+                paramsSerializer: function (params) {
+                  return Qs.stringify(params, {
+                    arrayFormat: 'brackets'
+                  })
+                },
+                params: {
+                  reqUrl: 'https://api.petfinder.com/pet.find',
+                  params: {
+                    key: '729776b0ff12f97426ef03d015026841',
+                    format: 'json',
+                    output: 'full',
+                    location: location,
+                    animal: type,
+                    age: age,
+                    count: 100,
+                  },
+                  proxyHeaders: {
+                    'header_params': 'value'
+                  },
+                  xmlToJSON: false
+                }
+            }).then((res) => {
+                console.log(res.data.petfinder.pets);
+            })
+        // )
+    }
+    
+    render() {
+        return (
+            <div className="App">
+                <Landing getPets={this.getPets}/>
+            </div>
+        );
+    }
 }
 
 export default App;
