71f19917acf2ecfcfe90b44faa1967fa98997cf8
Scott <girouard.scott@gmail.com>
2018-09-11 16:48:50 -0400

    sweet alerts

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SearchForm from './SearchForm';
+import { Link } from 'react-router-dom';
 
 class Landing extends Component {
     render() {
@@ -10,22 +11,27 @@ class Landing extends Component {
                     this.props.user
                     ? <button className="button" onClick={this.props.logout}>Log Out</button> 
                     : <button className="button" onClick={this.props.login}>Log In</button>
-                }   
+                } 
+                  {
+                    this.props.user && <Link className="link button header-button" to='/faves'>Faves</Link>
+                }  
                 </header>
 
         <div className="wrapper-landing"> 
                 <div className="landing-title">
                     <img src={require("./critter-logo.png")} className="landing-logo" />
+                    {/* <img src={require("./glitter-background.gif")} className="glitter" /> */}
+
                     {/* <h2 className="landing-tagline">Alternative Pets for Alternative People</h2> */}
                 </div>
 
             <div className="welcome-container clearfix">  
                 <div className="welcome box">
-                    <h3>welcome</h3>
+                    <h3>welcome<i class="fas fa-times"></i></h3>
                     <p>You're not just anyone. You're you. And you need a pet that's as unique as you. Welcome to CRITTER, where you can find the perfect adoptable pet in your area thats NOT a boring ol' dog or cat.</p>
                 </div>
                 <div className="instruction box">
-                    <h3>how it works</h3>
+                    <h3>how it works<i class="fas fa-times"></i></h3>
                     <ul>
                         <li>Step One: Make an account! You don't need an account if you just want to browse, but if you want to save the pet to your faves you're going to need an account!</li>
                         <li>Step Two: Search for pets! Enter your location or allow your browser to detect your location</li>
@@ -35,7 +41,7 @@ class Landing extends Component {
             </div>
 
                 <div className="landing-search-form box clearfix">
-                    <h3>search critters</h3>
+                    <h3>search critters<i class="fas fa-times"></i></h3>
                     <SearchForm location={this.props.location} breeds={this.props.breeds} getPets={this.props.getPets}/>
                 </div>
             </div>
